b1c7d789e1143970e7b025bb55d28b517251929e
ThuongMinhMai <thuongmtmse170103@fpt.edu.vn>
2023-10-05 15:31:25 +0700

    useEffect &ConText Hook

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery/dist/jquery.slim.min.js'
-import 'popper.js/dist/umd/popper.min.js'
-import 'bootstrap/dist/js/bootstrap.min.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "./components/ThemeContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.slim.min.js";
+import "popper.js/dist/umd/popper.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-    <App/>
-  
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
